513a7233e71008e2ab24680362969d22d74b81a9
Anna <helloboysandgirls.az@gmail.com>
2018-03-07 11:20:29 +0100

    Update SeleniumTestNG.java

--- src/testSelenium/SeleniumTestNG.java
+++ src/testSelenium/SeleniumTestNG.java
@@ -1,9 +1,7 @@
 package testSelenium;
 
-import org.testng.annotations.Test;
 public class SeleniumTestNG {
 	
-	@Test
 	public void Test1 () {
 		System.out.print("Mi primer test con Jenkins en selenium TestNG");
 	}
